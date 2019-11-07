eb04dba5cf139407392d03bc75eace66e4758063
Marc-André Denis <marc-andre.denis.3@ens.etsmtl.ca>
2019-11-06 21:00:24 -0500

    initial commit

---
+++ app/app_heartbeat.c
@@ -0,0 +1,16 @@
+#include "cmsis_os.h"
+#include "gpio.h"
+
+#include "main.h"
+#include "app_heartbeat.h"
+
+
+void app_heartbeat(void*arg)
+{
+//	osDelay(300000);
+//	EjectDrogue();
+    while (1) {
+        HAL_GPIO_TogglePin(LD4_GPIO_Port, LD4_Pin);
+        osDelay(100);
+    }
+}
---
+++ app/app_heartbeat.h
@@ -0,0 +1,6 @@
+#ifndef APP_HEARTBEAT_H_
+#define APP_HEARTBEAT_H_
+
+void app_heartbeat(void*arg);
+
+#endif
