eb4c810fe815f38505a534fc9b40074f57c5cfa4
Marc-Andre <marcadenis@msn.com>
2020-01-19 20:26:46 -0500

    Moved SD MISO to PB14

--- Inc/main.h
+++ Inc/main.h
@@ -61,14 +61,14 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define PH1_OSC_OUT_Pin GPIO_PIN_1
 #define PH1_OSC_OUT_GPIO_Port GPIOH
-#define SD_MISO_Pin GPIO_PIN_2
-#define SD_MISO_GPIO_Port GPIOC
 #define BOOT1_Pin GPIO_PIN_2
 #define BOOT1_GPIO_Port GPIOB
 #define SD_CS_Pin GPIO_PIN_12
 #define SD_CS_GPIO_Port GPIOB
 #define SD_CLK_Pin GPIO_PIN_13
 #define SD_CLK_GPIO_Port GPIOB
+#define SD_MISO_Pin GPIO_PIN_14
+#define SD_MISO_GPIO_Port GPIOB
 #define SD_MOSI_Pin GPIO_PIN_15
 #define SD_MOSI_GPIO_Port GPIOB
 #define LD4_Pin GPIO_PIN_15
