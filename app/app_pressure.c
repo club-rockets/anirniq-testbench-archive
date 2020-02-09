0a130efcb131ea4a0225ecf3e6b0971b7ee0a432
Zhogo <nathan.roussel.1@etsmtl.net>
2020-02-08 22:01:47 -0500

    Revert "Ajout de la lecture ADC"
    This reverts commit 027a94d1bf8f91cec9866fbc3fbafb077d89eb0a.

--- app/app_pressure.c
+++ app/app_pressure.c
@@ -7,50 +7,19 @@
 #include "app_pressure.h"
 
 extern ADC_HandleTypeDef hadc1;
-//Ajouter le mutex
 
 void tsk_pressure(void*arg){
     
-	osDelay(100);
-
-	int error = 0;
-	uint32_t value = 0;
-	float current = 0;
-	float resistance = 150;
-	float pressure = 0.;
-
+    osDelay(20);
 
     while(1){
 
-    	osDelay(200);
-
         //read adc
-    	value = (pressureGet(&hadc1)*3)/4095;
 
         //calculate pressure values
-    	pressure = 31.25*((value/resistance)-4);
 
         //save/send to sd card
 
-        //wait 10ms
-    	osDelay(10);
+        //wait 20ms
     }
-}
-
-uint32_t pressureGet(ADC_HandleTypeDef * adc){
-
-	uint32_t ADCValue = 0;
-
-	//Start to read ADC channel 1
-	if(HAL_ADC_Start(adc) == HAL_OK){
-
-		//Start
-		if(HAL_ADC_PollForConversion(adc,1000) == HAL_OK){
-
-			ADCValue = HAL_ADC_GetValue(adc);
-			HAL_ADC_Stop(adc);
-
-		}
-	}
-
 }
